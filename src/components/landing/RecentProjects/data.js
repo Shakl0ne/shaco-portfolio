77cac4262865c56ed22cc58d8eca76d58c32f3f9
Shakl0ne <934803719@qq.com>
2023-02-01 15:53:38 +0200

    ready to deploy

--- src/components/landing/RecentProjects/data.js
+++ src/components/landing/RecentProjects/data.js
@@ -3,21 +3,21 @@ export const RecentProjectData = [
     title: 'eCommerce',
     detail:
       'A full-stack ecommerce website using React & Redux and Python Django.',
-    image: process.env.PUBLIC_URL + '/images/eCommerce.png',
-    buttons: [{ label: 'Demo', link: '' }, { label: 'Code', link: 'https://github.com/Shakl0ne/Ecommerce' },],
+    image: process.env.PUBLIC_URL + '/images/ecommerce-thumbnail.png',
+    buttons: [{ label: 'Demo', link: 'https://shaco-ecommerce.herokuapp.com/' }, { label: 'Code', link: 'https://github.com/Shakl0ne/Ecommerce' },],
   },
   {
     title: 'whatsapp-clone',
     detail:
       'A simple SMS app using React and socket implementation.',
-    image: process.env.PUBLIC_URL + 'whatsapp-clone.png',
-    buttons: [{ label: 'Demo', link: '' }, { label: 'Code', link: 'https://github.com/Shakl0ne/whatsapp-clone' },],
+    image: process.env.PUBLIC_URL + '/images/whatsapp-clone-thumbnail.png',
+    buttons: [ { label: 'Code', link: 'https://github.com/Shakl0ne/whatsapp-clone' },],
   },
   {
     title: 'neuron',
     detail:
       'An data synchronization Java Quarkus project using with Kafka and Debezium.',
-    image: process.env.PUBLIC_URL + '/images/neuron.png',
+    image: process.env.PUBLIC_URL + '/images/neuron-thumbnail.png',
     buttons: [ { label: 'Code', link: 'https://github.com/Shakl0ne/neuron' }],
   },
 
